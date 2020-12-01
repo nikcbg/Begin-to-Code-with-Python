453c1beb8f1aadbfacde0ab4345d94010baa1653
nikcbg <nkcbg@yahoo.com>
2020-12-01 09:46:01 +0200

    add file

---
+++ 7. Using functions to simplify programs /EG7-16 Investigating the debugger.py
@@ -0,0 +1,13 @@
+# EG7-16 Investigating the debugger
+
+
+def increment_function(input_value):
+    result = input_value + 1
+    return result
+
+
+x = 99
+y = increment_function(x)
+print('The answer is:',y)
+
+
